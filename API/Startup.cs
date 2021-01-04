2ff4d1e22ce8f654468195d33dc65c2cdd2793f9
Walter Tudorin <wtudorin@gmail.com>
2021-01-05 08:51:07 +1100

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,7 @@ namespace API {
             services.AddSwaggerGen (c => {
                 c.SwaggerDoc ("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,10 +42,15 @@ namespace API {
                 app.UseSwagger ();
                 app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "API v1"));
             }
-
+ 
             app.UseHttpsRedirection ();
 
             app.UseRouting ();
+
+            app.UseCors( policy => policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization ();
 
